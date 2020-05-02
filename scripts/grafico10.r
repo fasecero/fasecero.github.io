a28f743f3cba9d312273543831aa3e667b65635d
fasecero <cpissaco@gmail.com>
2020-05-02 12:19:46 -0300

    Automatica 2020-05-02

--- scripts/grafico10.r
+++ scripts/grafico10.r
@@ -1,6 +1,12 @@
 #load libraries
 library(readxl);
 library(ggplot2);
+library(patchwork);
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
 
 #donwload file
 url <- "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide.xlsx";
@@ -23,7 +29,11 @@ grWidth <- grPixelWidth / grDpi;
 directorioInteractivo <- '~/covid/publica/fasecero.github.io'; 
 archivoInteractivo <- 'index.html';
 directorioGraficos <- 'graficos';
-pathGraficos <- paste(directorioInteractivo, '/', directorioGraficos, sep ="")
+pathGraficos <- paste(directorioInteractivo, '/', directorioGraficos, sep ="");
+#colores para los graficos de continentes: se mezclaban mucho el azul y el turquesa, el 4 es europa
+coloresContinente <- gg_color_hue(7);
+coloresContinente[4] <- 'red';
+
 
 #pre-process file
 
@@ -56,6 +66,19 @@ todosMayores = datoscovidok[datoscovidok$casesAcum >= casosABuscar,];
 tasasAnteriores <- merge(aggregate(casesAcum ~ countriesAndTerritories, data = todosMayores, FUN = min), todosMayores);
 #tasasAnteriores$tasaDiaria <- with(tasasAnteriores, (casesAcum/(casesAcum - casesAcumSemana))^(1/7)-1);
 tasasAnteriores <- aggregate(tasasAnteriores, by = list(tasasAnteriores$countriesAndTerritories), FUN = min);
+
+#continentes
+norte <- list('United_States_of_America', 'Canada', 'Mexico');
+datoscovidok$subContinente <- ifelse(datoscovidok$countriesAndTerritories %in% norte, 'North America (US, Canada, Mexico)', datoscovidok$continentExp);
+datoscovidok$subContinente <- ifelse(datoscovidok$subContinente == 'America', 'America Latina y Caribe', datoscovidok$subContinente);
+continente <- aggregate(datoscovidok[, c('cases', 'deaths', 'casesAcum', 'casesAcumSemana', 'deathsAcum')], by = list(datoscovidok$realDate, datoscovidok$subContinente), FUN = sum)
+#aggregate(casesAcum ~ countriesAndTerritories, data = todosMayores, FUN = min)
+poblacion <- aggregate(popData2018 ~ subContinente, data = datoscovidok[datoscovidok$realDate == as.Date('2020-04-27'),], FUN = sum);
+continente <- merge(continente, poblacion, by.x = 'Group.2', by.y ="subContinente", all.x = TRUE);
+continente$casosAcumCada100Mil <- continente$casesAcum / continente$popData2018 * 100000;
+continente$casosAcumSemanaCada100Mil <- continente$casesAcumSemana / continente$popData2018 * 100000;
+
+colores <- gg_color_hue(7);
 
 
   #grafico10
@@ -113,7 +136,6 @@ tasasAnteriores <- aggregate(tasasAnteriores, by = list(tasasAnteriores$countrie
     geom_text(aes(label = countriesAndTerritories), nudge_x = -0.01);
   
   #ggsave(paste('tasasanteriorescovid', ultimaFecha, ".png"), plot = grTasasAnteriores, width = grWidth, height = grHeight, dpi = grDpi, units = grUnits, device= grDevice);
-  library(patchwork);
   grTasasActuales <- grTasasActuales + labs(caption = element_blank());
   grTasasTotal <- grTasasActuales + grTasasAnteriores + plot_annotation(title = 'Comparacion de tasas');
   ggsave(paste(pathGraficos, '/', 'tasastotal', ultimaFecha, ".png", sep = ""), plot = grTasasTotal, width = grWidth, height = grHeight*2/3, dpi = grDpi, units = grUnits, device= grDevice);
@@ -161,9 +183,83 @@ tasasAnteriores <- aggregate(tasasAnteriores, by = list(tasasAnteriores$countrie
   if(file.exists(pathInteractivo)) unlink(pathInteractivo);
   if(file.copy(archivoInteractivo, directorioInteractivo)) unlink(archivoInteractivo);
 
+#graficos continentes
+
+grafico11 <- ggplot(
+  data = continente[continente$casesAcum > 1000, ], 
+  mapping = aes( 
+    x = casesAcum, 
+    y = casesAcumSemana, 
+    color = Group.2)) + 
+  geom_line() + 
+  scale_x_log10(labels = scales::comma_format()) + 
+  scale_y_log10(labels = scales::comma_format()) + 
+  theme(legend.position="bottom", legend.title  = element_blank()) +
+  labs(x = 'Casos confirmados', y = 'Casos semanales', caption = caption);
+
+grafico13 <- ggplot(
+  data = continente[continente$casesAcum > 1000, ], 
+  mapping = aes( 
+    x = casosAcumCada100Mil, 
+    y = casosAcumSemanaCada100Mil, 
+    color = Group.2)) + 
+  geom_line() + 
+  scale_x_log10(labels = scales::comma_format()) + 
+  scale_y_log10(labels = scales::comma_format()) + 
+  theme(legend.position="bottom", legend.title  = element_blank()) +
+  labs(x = 'Casos confirmados cada 100.000 habitantes', y = 'Casos semanales cada 100.000 habitantes', caption = caption);
+
+grafico12 <- ggplot(
+  data = continente[continente$Group.1 < (as.Date(max(continente$Group.1)) - 2),], 
+  mapping = aes( 
+    x = Group.1, 
+    y = deathsAcum, 
+    color = Group.2)) + 
+  geom_line() +
+  #geom_point() +
+  #scale_x_log10(labels =   scales::comma_format()) + 
+  scale_y_continuous(labels = scales::comma_format()) + 
+  theme(legend.position="bottom", legend.title  = element_blank()) +
+  labs(x = element_blank(), y = 'Muertes', caption = caption) + 
+  coord_cartesian(xlim = c(as.Date('2020-03-01'), as.Date(max(continente$Group.1))));
+
+grafico14 <- ggplot(
+  data = continente[continente$Group.1 < (as.Date(max(continente$Group.1)) - 2),], 
+  mapping = aes( 
+    x = Group.1, 
+    y = deathsAcum / popData2018 * 100000, 
+    color = Group.2)) + 
+  geom_line() +
+  #geom_point() +
+  #scale_x_log10(labels =   scales::comma_format()) + 
+  scale_y_continuous(labels = scales::comma_format()) + 
+  theme(legend.position="bottom", legend.title  = element_blank()) +
+  labs(x = element_blank(), y = 'Muertes cada 100.000 habitantes', caption = caption) + 
+  coord_cartesian(xlim = c(as.Date('2020-03-01'), as.Date(max(continente$Group.1))));
+
+
+grafico12 <- grafico12 + scale_colour_manual(values = coloresContinente);
+grafico11 <- grafico11 + scale_colour_manual(values = coloresContinente);
+grafico13 <- grafico13 + scale_colour_manual(values = coloresContinente);
+grafico14 <- grafico14 + scale_colour_manual(values = coloresContinente);
+
+grafico12 <- grafico12 + labs(caption = element_blank());
+grafico15 <- grafico12 + grafico14;
+
+
+grafico12 <- grafico11 + labs(caption = element_blank());
+grafico16 <- grafico11 + grafico13;
+
+ggsave(paste(pathGraficos, '/', 'casosPorContinente', ultimaFecha, ".png", sep = ""), plot = grafico15, width = grWidth, height = grHeight*2/3, dpi = grDpi, units = grUnits, device= grDevice);
+ggsave(paste(pathGraficos, '/', 'muertesPorContinente', ultimaFecha, ".png", sep = ""), plot = grafico16, width = grWidth, height = grHeight*2/3, dpi = grDpi, units = grUnits, device= grDevice);
+
+
+
+
 #system('cd ~/covid/publica/fasecero.github.io');
 setwd(directorioInteractivo)
 system('git add .');
 system(paste('git commit -m\'Automatica ', ultimaFecha, '\'', sep = ""));
 system('git push origin master');
+
 
